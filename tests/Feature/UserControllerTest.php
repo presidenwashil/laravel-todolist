d299d8ae07f176e72fc78d2c639c4ef185be5371
raihan <raihanpresiden2003@gmail.com>
2023-11-20 22:16:40 +0800

    Membuat Middleware Member

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -64,4 +64,9 @@ class UserControllerTest extends TestCase
             ->assertSessionMissing("user");
     }
 
+    public function testLogoutGuest()
+    {
+        $this->post('/logout')
+            ->assertRedirect("/");
+    }
 }
